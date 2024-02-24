32be83ba2b695036aa3f11860a7c8b618afc9b3e
unknown <liliannekho@gmail.com>
2024-02-24 10:26:55 -0500

    updates to app.js

--- app-server.js
+++ app-server.js
@@ -15,7 +15,7 @@ app.use(favicon(path.join(__dirname, 'public', 'img','logo.png')))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/blogs', require('./routes/api/blogs'))
 // http://localhost:8000/api/blogs
-app.use('/api/users', require('./routes/api/users'))
+app.use('/api/users', require('./routes/api/users'))// I am listening for any route that starts with api/users
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
